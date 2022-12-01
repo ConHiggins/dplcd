efa08f794f59c10bd384be6f6eca6b41b5264310
Conor Higgins <wodowods@gmail.com>
2022-12-01 23:30:01 +0000

    test edit of post schema

--- src/Components/AllPosts/AllPosts.jsx
+++ src/Components/AllPosts/AllPosts.jsx
@@ -10,6 +10,7 @@ export default function AllPosts() {
             .fetch(
                 `*[_type == "post"]{
         title,
+        subtext,
         slug,
         mainImage{
           asset->{
