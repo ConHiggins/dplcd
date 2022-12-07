89f01b16ddbe8defc3b793ffbbffb4dce1dd7bb5
Conor Higgins <wodowods@gmail.com>
2022-12-07 18:19:27 +0000

    testing against  npm package for font bug

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -57,6 +57,7 @@ const Home = ({}) => {
                             content={"displaced"}
                             charIncInterval={40}
                         ></ShuffleText>
+                        <h1>displaced</h1>
                     </div>
                     <div className="home__showcase-post">
                         {scPostsData.map((post, index) => (
