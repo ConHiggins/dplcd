7f4a26059def3263c391dc2f13058074bfb57501
Conor Higgins <wodowods@gmail.com>
2022-12-19 17:18:27 +0000

    added showcase post body fetch map and began styling

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -32,9 +32,10 @@ const Home = ({}) => {
         sanityClient
             .fetch(
                 /// * all of type:
-                `*[_type == "showcase"]{  
+                `*[_type == "post"]{  
         title,
         subtext,
+        body,
         slug,
         mainImage{
           asset->{
@@ -50,51 +51,47 @@ const Home = ({}) => {
 
     return (
         <div className="home">
-            <video
-                className="home__bg-vid"
-                src={videoBG}
-                autoPlay
-                loop
-                muted
-                playsInline
-            />
             {scPostsData && (
                 <>
                     <div className="home__splash" style={bgStyle}>
-                        <ShuffleText
-                            className="home__title"
-                            content={"displaced."}
-                            charIncInterval={40}
-                        ></ShuffleText>
+                        <ShuffleText className="home__title" content={"displaced."} charIncInterval={40}></ShuffleText>
+                        <video className="home__bg-vid" src={videoBG} autoPlay loop muted playsInline />
                     </div>
+
                     <div className="home__showcase-post">
                         {scPostsData.map((post, index) => (
                             <>
                                 <div
                                     className="home__showcase-post-bg"
                                     style={{
-                                        backgroundImage: `url(${post.mainImage.asset.url})`,
+                                        backgroundImage: `url(${post.mainImage?.asset.url})`,
                                     }}
                                 ></div>
-                                <Link
-                                    className="home__showcase-post__link"
-                                    to={"/" + post.slug.current}
-                                    key={post.slug.current}
-                                >
+                                <Link className="home__showcase-post__link" to={"/" + post.slug.current} key={post.slug.current}>
                                     <span key={index}>
-                                        <img
-                                            className="home__showcase-post__image"
-                                            src={post.mainImage.asset.url}
-                                            alt=""
-                                        />
+                                        <img className="home__showcase-post__image" src={post.mainImage?.asset.url} alt="" />
                                         <span>
                                             <h1>{post.title}</h1>
                                         </span>
                                     </span>
                                 </Link>
-                                <h3>
+                                <h3 className="home__showcase-post__subtext">
                                     <em>{post.subtext}</em>
                                 </h3>
+                                <div className="home__showcase-post__body">
+                                    {post.body[0].children.map((item) => {
+                                        ///URL's are stored in .markDefs
+                                        ///If a markDef._key exists that matches the item's key
+                                        const markDefMatchingIndex = post.body[0].markDefs.findIndex((x) => x._key);
+
+                                        return markDefMatchingIndex !== "undefined" &&
+                                            post.body[0].markDefs[markDefMatchingIndex]._key === item.marks[0] ? ( ///Match the URL to the text
+                                            <a href={post.body[0].markDefs[markDefMatchingIndex].href}>{Object.values(item.text)}</a>
+                                        ) : (
+                                            Object.values(item.text)
+                                        );
+                                    })}
+                                </div>
                             </>
                         ))}
                     </div>
