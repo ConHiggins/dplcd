1b5dd4511539d19c3bbc93d5149b4982b7e7aeff
Conor Higgins <wodowods@gmail.com>
2023-01-04 17:00:56 +0000

    grid format for Home showcase post

--- displaced/schemas/post.js
+++ displaced/schemas/post.js
@@ -14,6 +14,11 @@ export default {
             type: "string",
         },
         {
+            name: "client",
+            title: "Client",
+            type: "string",
+        },
+        {
             name: "slug",
             title: "Slug",
             type: "slug",
