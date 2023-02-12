524591e5769f7c43f03dbe1a787612d055249471
Conor Higgins <wodowods@gmail.com>
2023-02-12 13:55:21 +0000

    fixed nav link button content refresh bug

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -4,9 +4,11 @@ import { Link, useLocation } from "react-router-dom";
 
 const NavBar = ({ invertBgStyle, setInvertBgStyle }) => {
     const pages = [
-        { label: "projects", to: "/all-posts", changeTo: 1 },
-        { label: "home", to: "/", changeTo: 0 },
+        { label: "projects", current: "/", to: "/all-posts", changeTo: 1 },
+        { label: "home", current: "/all-posts", to: "/", changeTo: 0 },
     ];
+
+    const location = useLocation();
     const [buttonContent, setButtonContent] = useState(pages[0]);
 
     const handleContentChange = () => {
@@ -14,7 +16,9 @@ const NavBar = ({ invertBgStyle, setInvertBgStyle }) => {
     };
 
     useEffect(() => {
-        handleContentChange();
+        if (buttonContent.current !== location.pathname) {
+            handleContentChange();
+        }
     }, []);
 
     return (
