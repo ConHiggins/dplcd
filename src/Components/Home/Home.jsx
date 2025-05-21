ca3acdb85da012f1e1644b95d2826b9a87524d55
Thomas Walsh <82677648+thomaswalsh92@users.noreply.github.com>
2025-05-21 14:38:58 +0100

    Testing gsap smooth scroll

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,92 +1,172 @@
-import React, { useEffect, useState, useRef, createContext, useContext } from "react";
+//react
+import React, {
+  useEffect,
+  useState,
+  useRef,
+  createContext,
+  useContext,
+} from "react";
+import { Link } from "react-router-dom";
+
+//gsap
 import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
-import { Link } from "react-router-dom";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
+
+//three
+import MyThree from "../THREE/MyThree.jsx";
+
+//components
 import Button from "../Button/Button.jsx";
 import Loading from "../Loading/Loading.jsx";
 import Footer from "../Footer/Footer.jsx";
 import SinglePost from "../SinglePost/SinglePost.jsx";
 import FolioPost from "../folioPost/folioPost.jsx";
-import MyThree from "../THREE/MyThree.jsx";
+
+//css
 import "./Home.scss";
 
+gsap.registerPlugin(useGSAP, ScrollTrigger, ScrollSmoother);
+
 const Home = ({ handleSetLinkColour, scPostsData, bgStyle }) => {
-    const splashContentArray = [
-        "dsplaced.",
-        "dsplaced. is a multidisciplinary creative practice producing work that spans creative-direction, filmmaking, photography, music production, sound design, consulting and more.",
-    ];
+  const splashContentArray = [
+    "dsplaced.",
+    "dsplaced. is a multidisciplinary creative practice producing work that spans creative-direction, filmmaking, photography, music production, sound design, consulting and more.",
+  ];
 
-    const [displayLoading, setDisplayLoading] = useState(false);
-    // const [splashContentIndex, setSplashContentIndex] = useState(0);
-    // const [splashContentClass, setSplashContentClass] = useState("home__splash-content inactive");
-    const [folioIndex, setFolioIndex] = useState(0);
+  const [displayLoading, setDisplayLoading] = useState(false);
+  // const [splashContentIndex, setSplashContentIndex] = useState(0);
+  // const [splashContentClass, setSplashContentClass] = useState("home__splash-content inactive");
+  const [folioIndex, setFolioIndex] = useState(0);
 
-    //const [width, setWidth] = useState(window.innerWidth);
-    const linkCol = null; // useContext(linkColContext);
+  //const [width, setWidth] = useState(window.innerWidth);
+  const linkCol = null; // useContext(linkColContext);
 
-    const folioPosts = scPostsData
-        .map((p) => {
-            if (p.isFolio) return <FolioPost post={p} playVid={displayLoading} />;
-        })
-        .filter((p) => {
-            return p;
-        });
-
-    // function handleWindowSizeChange() {
-    //     setWidth(window.innerWidth);
-    // }
-    // useEffect(() => {
-    //     window.addEventListener("resize", handleWindowSizeChange);
-    //     return () => {
-    //         window.removeEventListener("resize", handleWindowSizeChange);
-    //     };
-    // }, []);
-
-    useEffect(() => {
-        const mainTimeout = setTimeout(() => {
-            setDisplayLoading(false);
-        }, 10000);
+  const folioPosts = scPostsData
+    .map((p) => {
+      if (p.isFolio) return <FolioPost post={p} playVid={displayLoading} />;
+    })
+    .filter((p) => {
+      return p;
     });
 
-    // ------------- GSAP ------------------------
+  // function handleWindowSizeChange() {
+  //     setWidth(window.innerWidth);
+  // }
+  // useEffect(() => {
+  //     window.addEventListener("resize", handleWindowSizeChange);
+  //     return () => {
+  //         window.removeEventListener("resize", handleWindowSizeChange);
+  //     };
+  // }, []);
 
-    const handleFolioUpdate = () => {
-        setFolioIndex((folioIndex) => (folioIndex >= folioPosts.length - 1 ? 0 : folioIndex + 1));
-    };
+  //   useEffect(() => {
+  //     const mainTimeout = setTimeout(() => {
+  //       setDisplayLoading(false);
+  //     }, 10000);
+  //   });
 
-    const container = useRef();
-    let postTween;
+  // ------------- GSAP ------------------------
 
-    useGSAP(() => {
-        postTween = gsap.to(".folio-post", {
-            delay: 10,
-            duration: 10,
-            onComplete: () => handleFolioUpdate(),
-        });
+  const handleFolioUpdate = () => {
+    // setFolioIndex((folioIndex) =>
+    //   folioIndex >= folioPosts.length - 1 ? 0 : folioIndex + 1
+    // );
+  };
+
+  const wrapper = useRef();
+  const content = useRef();
+
+  //   const scrollTo = () => {
+  //     smoother.current.scrollTo(".box-c", true, "center center");
+  //   };
+
+  useGSAP(
+    () => {
+      // create the smooth scroller FIRST!
+      wrapper.current = ScrollSmoother.create({
+        wrapper: wrapper.current,
+        content: content.current,
+        smooth: 2, // seconds it takes to "catch up" to native scroll position
+        effects: true, // look for data-speed and data-lag attributes on elements and animate accordingly
+      });
+    },
+    { scope: wrapper }
+  );
+
+  useGSAP(() => {
+    gsap.from("#dummy1", {
+      y: 200,
+      scrollTrigger: {
+        trigger: ".dummy-image-container",
+        start: "top bottom",
+        end: "+=950",
+        scrub: true,
+        markers: true,
+      },
     });
-
-    return (
+    gsap.from("#dummy2", {
+      y: 380,
+      scrollTrigger: {
+        trigger: ".dummy-image-container",
+        start: "top bottom",
+        end: "+=950",
+        scrub: true,
+        markers: true,
+      },
+    });
+    gsap.from("#dummy3", {
+      y: 560,
+      scrollTrigger: {
+        trigger: ".dummy-image-container",
+        start: "top bottom",
+        end: "+=950",
+        scrub: true,
+        markers: true,
+      },
+    });
+  });
+  return (
+    <div id="smooth-wrapper" ref={wrapper}>
+      <div id="smooth-content" ref={content}>
         <>
-            <div className="home">
-                {/* {displayLoading && <MyThree />} */}
-                <div className="info__desc"></div>
+          <div className="home">
+            <div className="dummy-video"></div>
+            <div className="dummy-image-container">
+              <div className="dummy-image" id="dummy1"></div>
+              <div className="dummy-image" id="dummy2"></div>
+              <div className="dummy-image" id="dummy3"></div>
             </div>
-            {scPostsData && (
-                <div
-                    className="folio-post-container"
-                    ref={container}
-                    onClick={() => {
-                        handleFolioUpdate();
-                        handleSetLinkColour(folioPosts[folioIndex].props.post.linkCol, linkCol);
-                    }}
-                >
-                    {folioPosts[folioIndex]}
-                </div>
-            )}
-
-            <Footer linkCol={linkCol} />
+            <div className="dummy-video"></div>
+          </div>
         </>
-    );
+      </div>
+    </div>
+    // <>
+    //   <div className="home">
+    //     {/* {displayLoading && <MyThree />} */}
+    //     <div className="info__desc"></div>
+    //   </div>
+    //   {scPostsData && (
+    //     <div
+    //       className="folio-post-container"
+    //       ref={container}
+    //       onClick={() => {
+    //         handleFolioUpdate();
+    //         handleSetLinkColour(
+    //           folioPosts[folioIndex].props.post.linkCol,
+    //           linkCol
+    //         );
+    //       }}
+    //     >
+    //       {folioPosts[folioIndex]}
+    //     </div>
+    //   )}
+
+    //   <Footer linkCol={linkCol} />
+    // </>
+  );
 };
 
 export default Home;
