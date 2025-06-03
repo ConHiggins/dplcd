b506f489393770e45c4a3e1eab7cc321d1b4d057
Thomas Walsh <82677648+thomaswalsh92@users.noreply.github.com>
2025-06-03 16:33:53 +0100

    Checkpoint working difference blend mode for header

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -2,62 +2,53 @@ import "./NavBar.scss";
 import React, { useEffect, useState, useContext } from "react";
 import { Link, useLocation } from "react-router-dom";
 
-const NavBar = ({ invertBgStyle, setInvertBgStyle, linkCol }) => {
-    const pages = [
-        { label: "PROJECTS", current: "/", to: "/projects", changeTo: 1 },
-        { label: "HOME", current: "/projects", to: "/", changeTo: 0 },
-    ];
+const NavBar = ({ context }) => {
+  const pages = [
+    { label: "SHOP", to: "/shop" },
+    { label: "INFO", to: "/info" },
+    { label: "PROJECTS", to: "/projects" },
+  ];
 
-    const location = useLocation();
-    const [buttonContent, setButtonContent] = useState(pages[0]);
-    const [col, setCol] = useState({color: "#0c1014"});
+  const location = useLocation();
 
-    const handleContentChange = () => {
-        setCol(buttonContent.changeTo == 1 ? {color: "#ffffff"} : {color: "#0c1014"});
-        setButtonContent(pages[buttonContent.changeTo]);
-    };
+  console.log(location);
 
-    useEffect(() => {
-        if ( buttonContent.current !== location.pathname) {
-            handleContentChange();
-        }
-    }, [location]);
+  // const pages = [
+  //     { label: "PROJECTS", current: "/", to: "/projects", changeTo: 1 },
+  //     { label: "HOME", current: "/projects", to: "/", changeTo: 0 },
+  // ];
 
-    return (
-        <>
-            {buttonContent.changeTo === 0 && (
-                <div className="navbar">
-                    <Link to={"/"} style={col} className="global_link navbar__title">
-                        dsplaced.
-                    </Link>
-                    <Link
-                        to={buttonContent.to}
-                        style={col}
-                        className="global_link navbar__link navbar__home"
-                        onClick={handleContentChange}
-                    >
-                        {buttonContent.label}
-                    </Link>
-                </div>
-            )}
-            {buttonContent.changeTo === 1 && (
-                <div className="global_link navbar" style={invertBgStyle}>
-                    <Link to={"/"} style={col} className="global_link navbar__title">
-                        dsplaced.
-                    </Link>
+  // const location = useLocation();
+  // const [buttonContent, setButtonContent] = useState(pages[0]);
+  // const [col, setCol] = useState({color: "#0c1014"});
 
-                    <Link
-                        to={buttonContent.to}
-                        style={col}
-                        className="global_link navbar__link navbar__home"
-                        onClick={handleContentChange}
-                    >
-                        {buttonContent.label}
-                    </Link>
-                </div>
-            )}
-        </>
-    );
+  // const handleContentChange = () => {
+  //     setCol(buttonContent.changeTo == 1 ? {color: "#ffffff"} : {color: "#0c1014"});
+  //     setButtonContent(pages[buttonContent.changeTo]);
+  // };
+
+  // useEffect(() => {
+  //     if ( buttonContent.current !== location.pathname) {
+  //         handleContentChange();
+  //     }
+  // }, [location]);
+
+  return (
+    <>
+      <div className="navbar">
+        <Link to={"/"} className="global_link navbar__title">
+          <span>dsplaced.</span>
+        </Link>
+        {pages.map((page) => {
+          return (
+            <Link to={page.to} className="global_link navbar__link">
+              {page.label}
+            </Link>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default NavBar;
