3b12fe0f42b090ff095da59fc13947baa5289fa7
Thomas Walsh <82677648+thomaswalsh92@users.noreply.github.com>
2025-08-18 09:53:37 +0100

    Home  page cleanup

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -58,365 +58,245 @@ const Home = ({ scPostsData }) => {
       wrapper.current = ScrollSmoother.create({
         wrapper: wrapper.current,
         content: content.current,
-        smooth: 3,
+        smooth: 1.5,
         effects: true,
       });
     },
     { scope: wrapper }
   );
 
-  useGSAP(() => {
-    let mm = gsap.matchMedia();
-
-    //fractal-anims
-    const pinVal = "+=1440";
-
-    mm.add("(min-width: 768px)", () => {
-      gsap.from("#fractel-1", {
-        y: 450,
-        scrollTrigger: {
-          trigger: "#fractel-image-section",
-          start: "top bottom",
-          end: "+=1080",
-          scrub: true,
-          markers: false,
-        },
-      });
-
-      gsap.from("#fractel-2", {
-        y: 750,
-        scrollTrigger: {
-          trigger: "#fractel-image-section",
-          start: "top bottom",
-          end: "+=1080",
-          scrub: true,
-          markers: false,
-        },
-      });
-
-      gsap.from("#fractel-3", {
-        y: 1050,
-        scrollTrigger: {
-          trigger: "#fractel-image-section",
-          start: "top bottom",
-          end: "+=1080",
-          scrub: true,
-          markers: false,
-        },
-      });
-
-      gsap.from("#fractel-images", {
-        scrollTrigger: {
-          trigger: "#fractel-image-section",
-          start: "top top",
-          end: pinVal,
-          scrub: true,
-          pin: true,
-        },
-      });
-
-      //my-protein-anims
-      gsap.from("#my-protein-1", {
-        y: 1050,
-        scrollTrigger: {
-          trigger: "#my-protein-image-section",
-          start: "top bottom",
-          end: "+=1080",
-          scrub: true,
-        },
-      });
-
-      gsap.from("#my-protein-2", {
-        y: 750,
-        scrollTrigger: {
-          trigger: "#my-protein-image-section",
-          start: "top bottom",
-          end: "+=1080",
-          scrub: true,
-        },
-      });
-
-      gsap.from("#my-protein-3", {
-        y: 450,
-        scrollTrigger: {
-          trigger: "#my-protein-image-section",
-          start: "top bottom",
-          end: "+=1080",
-          scrub: true,
-        },
-      });
-
-      gsap.from("#my-protein-images", {
-        scrollTrigger: {
-          trigger: "#my-protein-image-section",
-          start: "top top",
-          end: pinVal,
-          scrub: true,
-          pin: true,
-        },
-      });
-
-      //stillness-anims
-      gsap.from("#stillness-video-container", {
-        scrollTrigger: {
-          trigger: "#stillness-video-container",
-          start: "top top",
-          end: pinVal,
-          scrub: true,
-          pin: true,
-        },
-      });
-
-      //alan-johnson-anims
-      gsap.from("#alan-johnson-1", {
-        y: 450,
-        scrollTrigger: {
-          trigger: "#alan-johnson-image-section",
-          start: "top bottom",
-          end: "+=1080",
-          scrub: true,
-        },
-      });
-
-      gsap.from("#alan-johnson-2", {
-        y: 750,
-        scrollTrigger: {
-          trigger: "#alan-johnson-image-section",
-          start: "top bottom",
-          end: "+=1080",
-          scrub: true,
-        },
-      });
-
-      gsap.from("#alan-johnson-3", {
-        y: 1050,
-        scrollTrigger: {
-          trigger: "#alan-johnson-image-section",
-          start: "top bottom",
-          end: "+=1080",
-          scrub: true,
-        },
-      });
-
-      gsap.from("#alan-johnson-images", {
-        scrollTrigger: {
-          trigger: "#alan-johnson-image-section",
-          start: "top top",
-          end: pinVal,
-          scrub: true,
-          pin: true,
-        },
-      });
-
-      //highline-zine-anims
-      gsap.from("#highline-zine-1", {
-        y: 1050,
-        scrollTrigger: {
-          trigger: "#highline-zine-image-section",
-          start: "top bottom",
-          end: "+=1080",
-          scrub: true,
-        },
-      });
-
-      gsap.from("#highline-zine-2", {
-        y: 750,
-        scrollTrigger: {
-          trigger: "#highline-zine-image-section",
-          start: "top bottom",
-          end: "+=1080",
-          scrub: true,
-        },
-      });
-
-      gsap.from("#highline-zine-3", {
-        y: 450,
-        scrollTrigger: {
-          trigger: "#highline-zine-image-section",
-          start: "top bottom",
-          end: "+=1080",
-          scrub: true,
-        },
-      });
-
-      gsap.from("#highline-zine-images", {
-        scrollTrigger: {
-          trigger: "#highline-zine-image-section",
-          start: "top top",
-          end: pinVal,
-          scrub: true,
-          pin: true,
-        },
-      });
-
-      //bop anims
-      gsap.from("#bop-video", {
-        scrollTrigger: {
-          trigger: "#bop-video",
-          start: "top top",
-          end: pinVal,
-          scrub: true,
-          pin: true,
-          markers: false,
-        },
-      });
-
-      //highline video anims
-      gsap.from("#highline-dsplaced-video", {
-        scrollTrigger: {
-          trigger: "#highline-dsplaced-video",
-          start: "top top",
-          end: pinVal,
-          scrub: true,
-          pin: true,
-          markers: false,
-        },
-      });
-
-      //fthmless anims
-      gsap.from("#fthmlss-video", {
-        scrollTrigger: {
-          trigger: "#fthmlss-video",
-          start: "top top",
-          end: pinVal,
-          scrub: true,
-          pin: true,
-          markers: false,
-        },
-      });
-    });
-  });
-
   return (
     <div id="smooth-wrapper" ref={wrapper}>
       <div id="smooth-content" ref={content}>
         <div className="home">
-          <div id="fractel-video">
-            <div id="fractel-video-container">
-              <HomeVideo type="video" post={fractelXDsplaced} playVid={true} />
-            </div>
-          </div>
-          <div className="image-section" id="fractel-image-section">
-            <div className="image-container" id="fractel-images">
-              <div id="fractel-1">
-                <HomeImage
-                  image={fractelXDsplaced.imageStack[0]}
-                  imageClass="four-by-five-image"
-                />
-              </div>
-              <div id="fractel-2">
-                <HomeImage
-                  image={fractelXDsplaced.imageStack[1]}
-                  imageClass="four-by-five-image"
-                />
-              </div>
-              <div id="fractel-3">
-                <HomeImage
-                  image={fractelXDsplaced.imageStack[2]}
-                  imageClass="four-by-five-image"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="image-section" id="my-protein-image-section">
-            <div className="image-container" id="my-protein-images">
-              <div id="my-protein-1">
-                <HomeImage
-                  image={myProtein.imageStack[1]}
-                  imageClass="four-by-five-image"
-                />
-              </div>
-              <div id="my-protein-2">
-                <HomeImage
-                  image={myProtein.imageStack[0]}
-                  imageClass="four-by-five-image"
-                />
-              </div>
-              <div id="my-protein-3">
-                <HomeImage
-                  image={myProtein.imageStack[2]}
-                  imageClass="four-by-five-image"
-                />
-              </div>
-            </div>
-          </div>
-          <div id="stillness-video-container">
-            <div id="stillness-video">
-              <HomeVideo
-                videoStyle={{ height: "100vh" }}
-                post={ajStillness}
-                playVid={true}
-              />
-            </div>
-          </div>
-          <div className="image-section" id="alan-johnson-image-section">
-            <div className="image-container" id="alan-johnson-images">
-              <div id="alan-johnson-1">
-                <HomeImage
-                  image={ajProfile.imageStack[0]}
-                  imageClass="one-by-one-image"
-                />
-              </div>
-              <div id="alan-johnson-2">
-                <HomeImage
-                  image={ajProfile.imageStack[1]}
-                  imageClass="one-by-one-image"
-                />
-              </div>
-              <div id="alan-johnson-3">
-                <HomeImage
-                  image={ajProfile.imageStack[2]}
-                  imageClass="one-by-one-image"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="image-section" id="highline-zine-image-section">
-            <div className="image-container" id="highline-zine-images">
-              <div id="highline-zine-1">
-                <HomeImage
-                  image={highlineZine.imageStack[4]}
-                  imageClass="four-by-five-image"
-                />
-              </div>
-              <div id="highline-zine-2">
-                <HomeImage
-                  image={highlineZine.imageStack[0]}
-                  imageClass="four-by-five-image"
-                />
-              </div>
-              <div id="highline-zine-3">
-                <HomeImage
-                  image={highlineZine.imageStack[7]}
-                  imageClass="four-by-five-image"
-                />
-              </div>
-            </div>
-          </div>
-          <div id="bop-video">
-            <div id="bop-video-container">
-              <HomeVideo type="video" post={bop} playVid={true} />
-            </div>
-          </div>
           <div></div>
-          <div id="highline-dsplaced-video">
-            <div id="highline-dsplaced-video-container">
-              <HomeVideo
-                type="video"
-                post={highlineDsplacedVideo[0]}
-                playVid={true}
-              />
-            </div>
-          </div>
-          <div id="fthmlss-video">
-            <div id="fthmlss-video-container">
-              <HomeVideo type="video" post={fthmlssVideo[0]} playVid={true} />
-            </div>
-          </div>
         </div>
       </div>
     </div>
   );
 };
+
+//!!! Keep GSAP Snips for later ref
+// useGSAP(() => {
+//   let mm = gsap.matchMedia();
+
+//   //fractal-anims
+//   const pinVal = "+=1440";
+
+//   mm.add("(min-width: 768px)", () => {
+//     gsap.from("#fractel-1", {
+//       y: 450,
+//       scrollTrigger: {
+//         trigger: "#fractel-image-section",
+//         start: "top bottom",
+//         end: "+=1080",
+//         scrub: true,
+//         markers: false,
+//       },
+//     });
+
+//     gsap.from("#fractel-2", {
+//       y: 750,
+//       scrollTrigger: {
+//         trigger: "#fractel-image-section",
+//         start: "top bottom",
+//         end: "+=1080",
+//         scrub: true,
+//         markers: false,
+//       },
+//     });
+
+//     gsap.from("#fractel-3", {
+//       y: 1050,
+//       scrollTrigger: {
+//         trigger: "#fractel-image-section",
+//         start: "top bottom",
+//         end: "+=1080",
+//         scrub: true,
+//         markers: false,
+//       },
+//     });
+
+//     gsap.from("#fractel-images", {
+//       scrollTrigger: {
+//         trigger: "#fractel-image-section",
+//         start: "top top",
+//         end: pinVal,
+//         scrub: true,
+//         pin: true,
+//       },
+//     });
+
+//     //my-protein-anims
+//     gsap.from("#my-protein-1", {
+//       y: 1050,
+//       scrollTrigger: {
+//         trigger: "#my-protein-image-section",
+//         start: "top bottom",
+//         end: "+=1080",
+//         scrub: true,
+//       },
+//     });
+
+//     gsap.from("#my-protein-2", {
+//       y: 750,
+//       scrollTrigger: {
+//         trigger: "#my-protein-image-section",
+//         start: "top bottom",
+//         end: "+=1080",
+//         scrub: true,
+//       },
+//     });
+
+//     gsap.from("#my-protein-3", {
+//       y: 450,
+//       scrollTrigger: {
+//         trigger: "#my-protein-image-section",
+//         start: "top bottom",
+//         end: "+=1080",
+//         scrub: true,
+//       },
+//     });
+
+//     gsap.from("#my-protein-images", {
+//       scrollTrigger: {
+//         trigger: "#my-protein-image-section",
+//         start: "top top",
+//         end: pinVal,
+//         scrub: true,
+//         pin: true,
+//       },
+//     });
+
+//     //stillness-anims
+//     gsap.from("#stillness-video-container", {
+//       scrollTrigger: {
+//         trigger: "#stillness-video-container",
+//         start: "top top",
+//         end: pinVal,
+//         scrub: true,
+//         pin: true,
+//       },
+//     });
+
+//     //alan-johnson-anims
+//     gsap.from("#alan-johnson-1", {
+//       y: 450,
+//       scrollTrigger: {
+//         trigger: "#alan-johnson-image-section",
+//         start: "top bottom",
+//         end: "+=1080",
+//         scrub: true,
+//       },
+//     });
+
+//     gsap.from("#alan-johnson-2", {
+//       y: 750,
+//       scrollTrigger: {
+//         trigger: "#alan-johnson-image-section",
+//         start: "top bottom",
+//         end: "+=1080",
+//         scrub: true,
+//       },
+//     });
+
+//     gsap.from("#alan-johnson-3", {
+//       y: 1050,
+//       scrollTrigger: {
+//         trigger: "#alan-johnson-image-section",
+//         start: "top bottom",
+//         end: "+=1080",
+//         scrub: true,
+//       },
+//     });
+
+//     gsap.from("#alan-johnson-images", {
+//       scrollTrigger: {
+//         trigger: "#alan-johnson-image-section",
+//         start: "top top",
+//         end: pinVal,
+//         scrub: true,
+//         pin: true,
+//       },
+//     });
+
+//     //highline-zine-anims
+//     gsap.from("#highline-zine-1", {
+//       y: 1050,
+//       scrollTrigger: {
+//         trigger: "#highline-zine-image-section",
+//         start: "top bottom",
+//         end: "+=1080",
+//         scrub: true,
+//       },
+//     });
+
+//     gsap.from("#highline-zine-2", {
+//       y: 750,
+//       scrollTrigger: {
+//         trigger: "#highline-zine-image-section",
+//         start: "top bottom",
+//         end: "+=1080",
+//         scrub: true,
+//       },
+//     });
+
+//     gsap.from("#highline-zine-3", {
+//       y: 450,
+//       scrollTrigger: {
+//         trigger: "#highline-zine-image-section",
+//         start: "top bottom",
+//         end: "+=1080",
+//         scrub: true,
+//       },
+//     });
+
+//     gsap.from("#highline-zine-images", {
+//       scrollTrigger: {
+//         trigger: "#highline-zine-image-section",
+//         start: "top top",
+//         end: pinVal,
+//         scrub: true,
+//         pin: true,
+//       },
+//     });
+
+//     //bop anims
+//     gsap.from("#bop-video", {
+//       scrollTrigger: {
+//         trigger: "#bop-video",
+//         start: "top top",
+//         end: pinVal,
+//         scrub: true,
+//         pin: true,
+//         markers: false,
+//       },
+//     });
+
+//     //highline video anims
+//     gsap.from("#highline-dsplaced-video", {
+//       scrollTrigger: {
+//         trigger: "#highline-dsplaced-video",
+//         start: "top top",
+//         end: pinVal,
+//         scrub: true,
+//         pin: true,
+//         markers: false,
+//       },
+//     });
+
+//     //fthmless anims
+//     gsap.from("#fthmlss-video", {
+//       scrollTrigger: {
+//         trigger: "#fthmlss-video",
+//         start: "top top",
+//         end: pinVal,
+//         scrub: true,
+//         pin: true,
+//         markers: false,
+//       },
+//     });
+//   });
+// });
 
 export default Home;
